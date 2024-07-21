8d9e1ebaa4d955e636e8e28172bd86e158507e31
tayobaki <109907707+tayobaki@users.noreply.github.com>
2024-07-21 18:30:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
-    removeButton.className = "remove-btn";
+     removeButton.classList.add = "remove-btn";
     removeButton.onclick = function () {
       taskList.removeChild(li);
     };
